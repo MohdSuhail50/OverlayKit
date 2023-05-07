67672d9ce07463457397aa36a815f34a51ff5641
Suhail <mohdsuhailraza50@gmail.com>
2023-05-07 16:54:50 +0530

    Overlay code added

--- Example/OverlayKit/ViewController.swift
+++ Example/OverlayKit/ViewController.swift
@@ -7,17 +7,55 @@
 //
 
 import UIKit
+import OverlayKit
 
 class ViewController: UIViewController {
 
+    private var json = """
+    {
+    "image" : {
+      "url" : "https://phlearn.com/wp-content/uploads/2019/03/fixed-ratio.png",
+      "height" : 250,
+      "width" : 353
+    },
+    "text" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id bibendum libero. Ut eleifend sit amet felis vitae aliquam. Morbi ut justo leo. Maecenas fringilla justo id urna ornare",
+    "buttons" : [
+      {
+        "text" : "cancel",
+        "redirect_url" : "https://www.google.com",
+        "size" : {
+          "height" : 45,
+          "width" : 80
+        }
+      },
+      {
+        "text" : "ok",
+        "redirect_url" : "https://www.wigzo.com",
+        "size" : {
+          "height" : 45,
+          "width" : 80
+        }
+      }
+    ]
+  }
+"""
+    
+    var overLay: Overlay?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        overLay = Overlay(data: json)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction private func buttonPressed(_ sender: UIButton) {
+        overLay?.createOverlay()
     }
 
 }
---
+++ Example/Pods/Target Support Files/OverlayKit/OverlayKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_OverlayKit : NSObject
+@end
+@implementation PodsDummy_OverlayKit
+@end
